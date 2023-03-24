6d03e4fe2fb27e12de9d1ee01f51c9f3bb71c57f
Philip Crowther <crowtherpb@gmail.com>
2023-03-24 16:01:01 -0400

    refactor(separate funcs): separate cost finding and seaming finding functions.

--- seam-carver.py
+++ seam-carver.py
@@ -7,12 +7,10 @@ import numpy as np
 def get_arguments():
     # init parser
     parser = argparse.ArgumentParser()
-
     # add arguments
     parser.add_argument('image', type=str)
     parser.add_argument('save_path', type=str)
     parser.add_argument('seams', type=int)
-    
     # return parsed args
     return parser.parse_args()
 
@@ -23,63 +21,53 @@ def main(image_path, save_path, seams):
     imsave(save_path, image)  
 
 def run(image):
-    # detect edges then find lowest cost seam and return image with seam removed
+    # detect edges then find lowest costs seam and return image with seam removed
     edges = filters.sobel(rgb2gray(image))
-    return remove(image, seam(edges))
+    costs = find_costs(edges)
+    path = find_seam(costs, edges)
+    return remove(image, path)
 
-def seam(edges):
+def find_costs(edges):
     # find minimum seam and return path
-    cost = edges.copy()
-
-    # construct array to track/store path origin
-    origin = np.zeros(edges.shape)
-    for i in range(len(origin[0])):
-        origin[0][i] = i
-
+    costs = np.copy(edges)
     for i in range(1, len(edges)):
         for j in range(len(edges[0])):
-            print(i, j)
-            # find lowest cost path from previous row and update path origin
-            # format: (value, i index of value, j index of value)  -  the indices are used to update origin
+            # find lowest cost path from previous row and update path costs
             if j == 0: # left edge
-                prev = [(cost[i-1][j], i-1, j), (cost[i-1][j+1], i-1, j+1)]
+                cost = min([costs[i-1][j], costs[i-1][j+1]])
             elif j == len(edges[0]) - 1: # right edge
-                print(j)
-                prev = [(cost[i-1][j-1], i-1, j-1), (cost[i-1][j], i-1, j)]
+                cost = min([costs[i-1][j-1], costs[i-1][j]])
             else:  # middle
-                prev = [(cost[i-1][j-1], i-1, j-1), (cost[i-1][j], i-1, j), (cost[i-1][j+1], i-1, j+1)]
-            # get lowest possible cost and its location
-            c, i1, j1 = min(prev, key=lambda x: x[0])
-            # update cost and origin
-            cost[i][j] += c
-            origin[i][j] = origin[i1][j1]
+                cost = min([costs[i-1][j-1], costs[i-1][j], costs[i-1][j+1]])
+            # update costs
+            costs[i][j] += cost
+    return costs
 
-    # identify index of minimum seam's lowest point
-    minimum, min_index = float('inf'), 0  
-    for i in range(len(edges[-1])):
-        if edges[-1][i] < minimum:
-            minimum, min_index = edges[-1][i], i
+def find_seam(costs, edges):
+    current_index = find_lowest(costs)
+    seam = [current_index]
+    for row in reversed(range(len(costs) - 1)):
+        if current_index == 0:
+            seam.append(np.argmin(costs[row][:2]) + seam[-1])
+        elif current_index == len(costs[row]):
+            seam.append(np.argmin(costs[row][-2:]) + seam[-1] -1)
+        else:
+            seam.append(np.argmin(costs[row][current_index-1:current_index+2]) + seam[-1] - 1)
+        current_index = seam[-1]
+    return seam
 
-    start = origin[-1][min_index]
-    # track minimum seam back to origin and store path
-    path, curr = [min_index], min_index
-    for i in reversed(range(len(origin))-1):  
-        if origin[i][curr] == start:
-            path.append(curr)
-        elif curr > 0 and origin[i][curr-1] == start:
-            curr -= 1
-            path.append(curr)
-        else:  # if neither of the other two options were it, then the last one  has to be
-            curr += 1
-            path.append(curr)
-
-    return path
-
+def find_lowest(costs):
+    # find final index of the lowest cost seam
+    lowest, lowest_index = float('inf'), 0
+    for i in range(len(costs[-1])):
+        if costs[-1][i] < lowest:
+            lowest, lowest_index = costs[-1][i], i
+    return lowest_index
 
 def remove(image, path):
     # remove one pixel at each index specified in path
-    num_rows, num_col = image.shape
-    new_image = np.zeros((num_rows, num_col - 1))
+    num_rows, num_col = image.shape[:2]
+    new_image = np.zeros((num_rows, num_col - 1, 3))
     for row in range(num_rows):
         new_image[row][:] = np.concatenate((image[row][:path[row]], image[row][path[row]+1:]))
     return new_image
@@ -89,3 +77,4 @@ if __name__ == "__main__":
     # get arguments
     args = get_arguments()
     main(args.image, args.save_path, args.seams)
+
